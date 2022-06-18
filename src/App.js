15827957be6130b6fbd9b2e8241e447728685ec2
Steve Suarez <suarezsteve21@gmail.com>
2022-06-18 12:27:25 -0500

    more changes

--- src/App.js
+++ src/App.js
@@ -9,9 +9,21 @@ import { Activities } from "./pages/Activities";
 function App() {
   const location = useLocation();
   return (
-    <div className="h-screen relative backgroundimage">
-      <nav class="bg-white border-gray-200 px-2 sm:px-4 py-4 dark:bg-gray-800">
-        <div class="container flex flex-wrap justify-between items-center mx-auto">
+    <div className="h-screen backgroundimage">
+      <nav class="bg-white border-gray-200 px-2 py-4 dark:bg-gray-800">
+        <div class="container flex flex-wrap space-x-4 items-center mx-auto">
+          {location.pathname !== "/" && (
+            <Link to="/" className="bg-white p-2 rounded-md text-white">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+              >
+                <path d="M12 9.185l7 6.514v6.301h-3v-5h-8v5h-3v-6.301l7-6.514zm0-2.732l-9 8.375v9.172h7v-5h4v5h7v-9.172l-9-8.375zm12 5.695l-12-11.148-12 11.133 1.361 1.465 10.639-9.868 10.639 9.883 1.361-1.465z" />
+              </svg>
+            </Link>
+          )}
           <span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white">
             Universidad EAN - Grupo 4
           </span>
@@ -52,21 +64,6 @@ function App() {
         </div>
       </nav>
 
-      {location.pathname !== "/" && (
-        <Link
-          to="/"
-          className="absolute top-5 left-5 bg-white p-4 rounded-md text-white"
-        >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="24"
-            height="24"
-            viewBox="0 0 24 24"
-          >
-            <path d="M12 9.185l7 6.514v6.301h-3v-5h-8v5h-3v-6.301l7-6.514zm0-2.732l-9 8.375v9.172h7v-5h4v5h7v-9.172l-9-8.375zm12 5.695l-12-11.148-12 11.133 1.361 1.465 10.639-9.868 10.639 9.883 1.361-1.465z" />
-          </svg>
-        </Link>
-      )}
       <div className="h-auto flex flex-col align-center  space-y-20">
         <Routes>
           <Route path="/" element={<Opciones />} />
