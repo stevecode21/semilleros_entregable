470f9ec22ba409b8662e362cc4ff0bac5334ff53
Steve Suarez <suarezsteve21@gmail.com>
2022-06-18 10:02:49 -0500

    Gestion actividades

--- src/App.js
+++ src/App.js
@@ -1,27 +1,28 @@
-import './App.css';
+import "./App.css";
 import { Routes, Route, Link } from "react-router-dom";
-import { Opciones } from './pages/Opciones';
-import { Student } from './pages/Student';
-import { Tutor } from './pages/Tutor';
-import { Semillero } from './pages/Semillero';
-
+import { Opciones } from "./pages/Opciones";
+import { Student } from "./pages/Student";
+import { Tutor } from "./pages/Tutor";
+import { Semillero } from "./pages/Semillero";
+import { Activities } from "./pages/Activities";
 
 function App() {
-
   return (
-    <div className='h-screen relative'>
-
-      <div className='absolute top-5 left-5 bg-green-700 px-10 rounded-md'>
-      <Link to="/" className='text-white'>Volver al home</Link>
+    <div className="h-screen relative">
+      <div className="absolute top-5 left-5 bg-green-700 px-10 rounded-md">
+        <Link to="/" className="text-white">
+          Volver al home
+        </Link>
       </div>
-    <div className="h-full flex flex-col align-center justify-center space-y-20">
-      <Routes>
-        <Route path="/" element={<Opciones/> } />
-        <Route path="student" element={<Student />} />
-        <Route path="tutor" element={<Tutor />} />
-        <Route path="semillero" element={<Semillero />} />
-      </Routes>
-    </div>
+      <div className="h-full flex flex-col align-center justify-center space-y-20">
+        <Routes>
+          <Route path="/" element={<Opciones />} />
+          <Route path="student" element={<Student />} />
+          <Route path="tutor" element={<Tutor />} />
+          <Route path="semillero" element={<Semillero />} />
+          <Route path="actividades" element={<Activities />} />
+        </Routes>
+      </div>
     </div>
   );
 }
---
+++ src/pages/Activities.js
@@ -0,0 +1,37 @@
+import React, { useState, useEffect } from "react";
+
+export const Activities = () => {
+  const [semilleros, setSemilleros] = useState([]);
+
+  useEffect(() => {
+    const storage = JSON.parse(localStorage.getItem("semilleros"));
+    console.log(
+      "🚀 ~ file: Activities.js ~ line 8 ~ useEffect ~ storage",
+      storage
+    );
+    setSemilleros(storage.semilleros || []);
+  }, []);
+
+  return (
+    <div className="w-full h-screen flex align-center justify-center">
+      <div className="grid grid-cols-4 gap-x-6 m-auto">
+        {semilleros.map((item, index) => {
+          return (
+            <div
+              className="rounded-md w-40 p-8 h-70 text-center mx-14 my-10 bg-blue-400 col-span-1"
+              key={index}
+            >
+              {/* name,
+        mainGoal,
+        specificGoals,
+        students: selectedStudents,
+        researchGroup: researchGroupSelected,
+        tutors: selectedTutors, */}
+              <p className="text-white">{item.name}</p>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
